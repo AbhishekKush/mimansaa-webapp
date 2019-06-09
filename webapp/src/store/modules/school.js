cbcaf99b7e5e08067232f3aca1c5d5d631dc6e8d
AbhishekKush <30891890+AbhishekKush@users.noreply.github.com>
2019-06-09 16:17:18 +0530

    worked on school module

--- webapp/src/store/modules/school.js
+++ webapp/src/store/modules/school.js
@@ -11,10 +11,69 @@ export default{
     getters:{
             
     },
-    mutations:{
+    mutations: {
+        addSchool(state, payload) {
 
+        },
+        editedSchoolName(state, payload) {
+            console.log("Payload " + payload)
+            state.loadedSchool.name = payload
     },
-    actions:{
-
+        setLoadedSchools(state, payload) {
+            state.loadedSchools = payload
+        }
+    },
+    actions: {
+        addNewSchool({ dispatch,state }, payload) {
+            state.loading = true
+            dispatch('showSnackbar', {
+                snackbar: {
+                    message: "Saving Data"
+                }
+            })
+            const ref = firebase.database().ref('info_school')
+            ref.push(payload)
+                .then((data) => {
+                    ref.child(data.key).update({ "id": data.key,"addedOn": firebase.database.ServerValue.TIMESTAMP })
+                    dispatch('showSnackbar', {
+                        snackbar: {
+                            message: "Data saved successfully",
+                            type: "success"
+                        }
+                    })
+                    state.loading = false
+                })
+                .catch((error) => {
+                    console.log(error)
+                    dispatch('showSnackbar', {
+                        snackbar: {
+                            message: "Error while saving data",
+                            type: "error"
+                        }
+                    })
+                    state.loading = false
+                })
+        },
+        loadSchools({ commit, state }) {
+            state.loading = true
+            firebase.database().ref('info_school').once('value')
+                .then((data) => {
+                    const schools = []
+                    const obj = data.val()
+                    for (let key in obj) {
+                        schools.push({
+                            id: key,
+                            name: obj[key].name,
+                            city: obj[key].city,
+                            phone: obj[key].phone
+                        })
+                    }
+                    commit('setLoadedSchools', schools)
+                    console.log(schools)
+                    state.loading = false
+                })
+        },
+        loadSchool({commit}){
+            
+        }
     }
-}
